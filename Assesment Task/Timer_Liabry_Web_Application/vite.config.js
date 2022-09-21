192e0e44af4fd56385b382892cf785877bbaff76
marsked <joshye4@gmail.com>
2022-09-22 08:20:57 +1000

    removed dist folder

--- Assesment Task/Timer_Liabry_Web_Application/vite.config.js
+++ Assesment Task/Timer_Liabry_Web_Application/vite.config.js
@@ -5,24 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    base: '/SIT-120/',
-    plugins: [vue()],
-    // poduction deployment to GitHub repo
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      
-      }
-    },
-   /* publicPath: process.env.NODE_ENV === 'production'
-    ? '/SIT-120/'
-    : '/'
-  }, */
+  base: '/SIT-120/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-}})
+})

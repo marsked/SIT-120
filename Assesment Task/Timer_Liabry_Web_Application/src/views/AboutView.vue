0ca62bb7c4ea39b7a71bbc861fbc38755298a6bc
marsked <joshye4@gmail.com>
2022-09-23 08:52:09 +1000

    commit

--- Assesment Task/Timer_Liabry_Web_Application/src/views/AboutView.vue
+++ Assesment Task/Timer_Liabry_Web_Application/src/views/AboutView.vue
@@ -2,11 +2,20 @@
 <template>
   <div>
     <h1>SIT120 Assesment 3 Project</h1>
-    <p>This application is a timer resource website for the use of those who may need selection of various timers for
+    <h3>By Joshua Erickson</h3>
+    <br>
+    <p>This <mark>responsive web application</mark> is a timer resource website for the use of those who may need selection of various timers for
       use by those who need easy access to such tools.</p>
     <p>First and foremost it will be, as mentioned above, a library of various timers for use in a number of
       applications (i.e. chess) sorted in a convenient and accessible format.</p>
+      <br>
     <p>It will also include functionality for users to report bugs and request new auditions as well as a change log in the future.
     </p>
   </div>
 </template>
+
+<style>
+  h1, h2, h3 {
+    color: var(--color-heading);
+  }
+</style>
